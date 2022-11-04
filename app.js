97e2b8c3bc6340ee66f0562855b0c8154365ad47
ltmj <lauri.jaakkonen@tuni.fi>
2022-11-04 12:17:47 +0200

    final commit, everything should work now

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.get('/version', (req, res) => {
 
 app.use(express.static('dist'))
 
+//start server
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
